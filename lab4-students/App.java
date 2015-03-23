4de64722e640accf6563d2330bc36070858db1d6
Nirmal Lankathilaka <nir3al@gmail.com>
2015-03-24 02:11:44 +0530

    Implementing new BTreeNode as Grade (devel)

--- lab4-students/App.java
+++ lab4-students/App.java
@@ -4,29 +4,46 @@ class App {
 
     final static String[] names={"T.M.S.U.B.Abeysinghe", "A.S.M.C.BAgalawatte", "W.G.N.M.Amarasinghe", "D.M.A.M.K.L.Andradi", "A.S.P.Athukorala", "B.A.N.Y.Balaarachchi", "K.Balakayan", "B.M.N.M.Balasooriya", "H.M.A.P.KBandara", "K.M.P.Bandara", "R.M.L.SBandara", "B.S.Bulathsinhala", "B.V.O.Chathuranga", "G.D.Chathuranga", "G.D.RDabarera", "W.T.Darshana", "J.P.T.Dhanushka", "U.W.T.DEhelepola", "C.N.Ekanayake", "M.S.D.Eranga", "P.AGregory", "D.L.Gunaratne", "K.A.R.L.A.Gunawardana", "S.D.T.Guruge", "M.G.Harischandra", "S.K.E.Harshani", "D.H.M.S.N.Herath", "T.D.Hirimuthugoda", "J.D.C.Jayalath", "D.N.Jayanath", "O.M.D.N.I.Jayathilaka", "K.E.G.A.P.Kadurugasyaya", "D.S.L.Karunasekara", "J.L.M.M.Jayawardena", "K.P.D.H.M.Karunatissa", "P.Keerthanan", "B.U.Kulathilaka", "Y.M.P.NLankathilaka", "M.M.D.B.Marasinghe", "N.B.U.SNanayakkara", "D.N.U.Naranpanawa", "K.N.M.Perera", "W.D.Y.NPiyasinghe", "W.M.S.Prasadima", "E.K.V.MPremaratne", "M.P.Prematilake", "R.M.P.Rajapaksha", "R.M.D.MRathnayake", "A.R.S.PRodrigo", "J.CSamarasinghe", "S.Selvaluxmiy", "A.S.D.Senevirathna", "V.Seralathan", "E.L.W.Shantha", "K.V.C.C.Somathilaka", "D.N.Tennakoon", "R.Velmakivan", "W.M.D.B.Weerakoon", "I.L.Weeraratne", "N.B.Weerasinghe", "W.M.T.G.I.S.Weerasooriya", "W.R.M.B.C.Wickramasinghe", "Y.A.D.S.S.Wijerathna", "K.W.H.N.T.Hettiarachchi", "W.T.S.Wehalla", "S.S.Manamendra", "M.B.K.Jayawardhana", "K.G.DE.A.Rajapakse"};
 
-    class Student<T> extends BTree {
-        double gpa=0.0;
+    class Student extends BTree implements Comparable<Student> {
 
-        public class BTreeNode<T extends Comparable<T>>{
+         class Grade implements Comparable<Grade> {
             
-            T data;
-            BTreeNode<T> left, right;
+            double grade;
+            String course;
 
-            BTreeNode(T data) {
-                this.data=data;
+            Grade(String data) { this.course=data; }
+
+            Grade(String course, double grade) {
+                this.course=course;
+                this.grade=grade;
                 left=null;
                 right=null;
             }
-        }
 
-        // @Override
-        public void in_order(BTreeNode<T> grade) {
-            // 
-        }
+            @Override
+            public int compareTo(Grade to) {
+                return (int) (10*(this.grade-to.grade)); // [] TODO: reverse??
+            }
+         } 
 
-        public int compareTo(Student to) {
-           return 0; 
-        }
+         Grade left, right, root;
+         double gpa=0.0;
+
+         // @Override
+         public void in_order(Grade grade) {
+            if (grade!=null) {
+                in_order(grade.left);
+                gpa+=grade.grade;
+                in_order(grade.right);
+            }
+         }
+
+         @Override
+         public int compareTo(Student to) {
+            in_order(root);
+            // [] TODO divide by number
+            return 0; // [] TODO implement
+         }
     };
 
 
